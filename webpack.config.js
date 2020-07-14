c82c72d39441a21c895ba531845390c53a3b618c
Gunjan Datta <me@dattabase.com>
2020-07-14 19:13:57 -0400

    Updated webpack for distribution files.

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,75 @@ var path = require("path");
 var { VueLoaderPlugin } = require("vue-loader");
 
 // Return the configuration
-module.exports = {
-    // Include the gd-sprest global library
-    entry: "./src/index.ts",
+module.exports = (env, argv) => {
+    return {
+        // Include the gd-sprest global library
+        entry: "./src/index.ts",
 
-    // Output location
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js"
-    },
+        // Output location
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "app" + (argv.mode === "development" ? "" : ".min") + ".js"
+        },
 
-    // Resolve the file names
-    resolve: {
-        extensions: [".js", ".css", ".scss", ".ts", ".vue"]
-    },
+        // Resolve the file names
+        resolve: {
+            extensions: [".js", ".css", ".scss", ".ts", ".vue"]
+        },
 
-    // Dev Server
-    devServer: {
-        inline: true,
-        hot: true,
-        open: true,
-        publicPath: "/dist/"
-    },
+        // Dev Server
+        devServer: {
+            inline: true,
+            hot: true,
+            open: true,
+            publicPath: "/dist/"
+        },
 
-    // Plugins
-    plugins: [
-        new VueLoaderPlugin()
-    ],
+        // Plugins
+        plugins: [
+            new VueLoaderPlugin()
+        ],
 
-    // Loaders
-    module: {
-        rules: [
-            // SASS to JavaScript
-            {
-                // Target the sass and css files
-                test: /\.s?css$/,
-                // Define the compiler to use
-                use: [
-                    // Create style nodes from the CommonJS code
-                    { loader: "style-loader" },
-                    // Translate css to CommonJS
-                    { loader: "css-loader" },
-                    // Compile sass to css
-                    { loader: "sass-loader" }
-                ]
-            },
-            // TypeScript to JavaScript
-            {
-                // Target TypeScript files
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    // JavaScript (ES5) -> JavaScript (Current)
-                    {
-                        loader: "babel-loader",
-                        options: { presets: ["@babel/preset-env"] }
-                    },
-                    // TypeScript -> JavaScript (ES5)
-                    { loader: "ts-loader" }
-                ]
-            },
-            // VueJS
-            {
-                // Target VueJS files
-                test: /\.vue$/,
-                // Vue Template -> JavaScript
-                loader: "vue-loader"
-            }
-        ]
-    }
+        // Loaders
+        module: {
+            rules: [
+                // SASS to JavaScript
+                {
+                    // Target the sass and css files
+                    test: /\.s?css$/,
+                    // Define the compiler to use
+                    use: [
+                        // Create style nodes from the CommonJS code
+                        { loader: "style-loader" },
+                        // Translate css to CommonJS
+                        { loader: "css-loader" },
+                        // Compile sass to css
+                        { loader: "sass-loader" }
+                    ]
+                },
+                // TypeScript to JavaScript
+                {
+                    // Target TypeScript files
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        // JavaScript (ES5) -> JavaScript (Current)
+                        {
+                            loader: "babel-loader",
+                            options: { presets: ["@babel/preset-env"] }
+                        },
+                        // TypeScript -> JavaScript (ES5)
+                        { loader: "ts-loader" }
+                    ]
+                },
+                // VueJS
+                {
+                    // Target VueJS files
+                    test: /\.vue$/,
+                    // Vue Template -> JavaScript
+                    loader: "vue-loader"
+                }
+            ]
+        }
+    };
 }
