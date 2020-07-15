bfe25e802160dd05d9d03e87eb959e1a6d60a683
Gunjan Datta <me@dattabase.com>
2020-07-14 21:15:26 -0400

    Updated the item view.

--- src/views/item.vue
+++ src/views/item.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <button @click="goBack">Back</button>
+    <Button v-bind:iconType="iconType" v-bind:type="btnType" v-bind:onClick="goBack" />
     <h1>Item {{ this.$route.params.id }}</h1>
   </div>
 </template>
 
 <script>
+import { Components, IconTypes } from "gd-sprest-bs";
+import { Button } from "gd-sprest-bs-vue";
 import { Views } from "../router";
 export default {
+  components: { Button },
+  data() {
+    return {
+      iconType: IconTypes.ArrowBarLeft,
+      btnType: Components.ButtonTypes.Secondary
+    }
+  },
   methods: {
     goBack() {
       Views.Home();
